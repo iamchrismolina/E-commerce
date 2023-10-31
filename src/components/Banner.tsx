d1231ca7581bcf9b55219ca01519daa7c410df6a
iamchrismolina <ragepool123@gmail.com>
2023-11-01 04:21:43 +0800

    fix: add responsive banner on all breakpoints

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,10 @@ const Banner = () => {
   return (
     <section className="m-auto container">
       <div className="bg-cover">
-        <img src="./images/banner/banner.jpg" className="h-64 w-full" />
+        <img
+          src="./images/banner/banner.jpg"
+          className="h-64 w-full lg:h-80 xl:h-96 object-cover"
+        />
       </div>
     </section>
   );
